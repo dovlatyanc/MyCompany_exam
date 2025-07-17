63652ecf9cc63847652a763ea44a75702c3e13ed
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-07-17 21:47:24 +0300

    добавил универсальные  категории и типы услуг связь один ко многим

---
+++ Domain/Entities/Service.cs
@@ -0,0 +1,28 @@
+﻿using MyCompany.Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities;
+
+public class Service : EntityBase
+{
+    [Display(Name = "Выберите категорию, к которой относится услуга")]
+    public int? SerrviceCategoryId { get; set; }
+    public ServiceCategory? ServiceCategory { get; set; }
+
+    [Display(Name = "Краткое описание")]
+    [MaxLength(3_000)]
+    public string? DescriptionShort { get; set; }
+
+    [Display(Name = "Описание")]
+    [MaxLength(100_000)]
+    public string? Description { get; set; }
+
+    [Display(Name = "Титульная картинка")]
+    public string? Photo { get; set; }
+
+    [Display(Name = "Тип услуги")]
+    public ServiceTypeEnum Type { get; set; }
+
+
+}
---
+++ Domain/Entities/ServiceCategory.cs
@@ -0,0 +1,7 @@
+﻿namespace MyCompany.Domain.Entities
+{
+    public class ServiceCategory:EntityBase
+    {
+        public ICollection<Service> Services { get; set; }
+    }
+}
