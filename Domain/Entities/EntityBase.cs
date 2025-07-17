7eeba6f4e118910d13ddcd75dd94a6f477e6d549
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-07-17 21:34:44 +0300

    добавил базовую сущность

---
+++ Domain/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public abstract  class EntityBase
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Заполните название")]
+        [Display (Name ="Название")]
+        [MaxLength(200)]
+        public string? Title { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
