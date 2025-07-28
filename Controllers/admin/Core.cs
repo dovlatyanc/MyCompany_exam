31056f2c07f335993f2eafa0f9071cf9964de38c
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-07-28 11:22:38 +0300

    Добавил админку и частично реализовал Админконтроллер

--- Controllers/admin/Core.cs
+++ Controllers/admin/Core.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 
 namespace MyCompany.Controllers.admin
 {
+    [Authorize(Roles ="admin")]
     public partial class AdminController:Controller
     {
+        private readonly DataManager _dataManager;
+        
+        public AdminController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Логин")]
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
