e22c8fc94ba13c8f185da036320dff0a9f4ebc93
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-07-28 18:38:20 +0300

    добавил представления для редактирования категорий с возможнгостью добавления изменения и удаления категорий на сайт и в БД

--- Controllers/admin/ServiceCategories.cs
+++ Controllers/admin/ServiceCategories.cs
@@ -1,6 +1,37 @@
-﻿namespace MyCompany.Controllers.admin
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain.Entities;
+
+namespace MyCompany.Controllers.admin
 {
     public partial class AdminController
     {
+        public async Task<IActionResult> ServiceCategoriesEdit(int id)
+        {
+            //зависимости от наличия ID добавляем либо изменяем запись
+            ServiceCategory? entity = id == default ? new ServiceCategory() : await _dataManager.ServiceCategories.GetServiceCategoryyIdAsync(id);
+
+            return View(entity);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesEdit(ServiceCategory entity)
+        {
+            // в модели присутствуют ошибки возыращаем на доработку
+            if (!ModelState.IsValid)
+                return View(entity);
+
+            await _dataManager.ServiceCategories.SaveServiceCategoryAsync(entity);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ServiceCategoriesDelete(int id)
+        {
+            // тк в целях безопасности отключено каскадное удаление убедитесь прежде чем удалить категори
+            // что на нее нет ссылак ни у одной  из услуг
+            await _dataManager.ServiceCategories.DeleteServiceCategoryAsync(id);
+
+            return RedirectToAction("Index");
+        }
     }
 }
