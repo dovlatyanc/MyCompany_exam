b2acd2a66965897858e557197097e84527b1f42e
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-08-01 17:34:47 +0300

    добавил клас для превращения service в ServiceDTO

---
+++ Infrastructure/HelperDTO.cs
@@ -0,0 +1,24 @@
+﻿using MyCompany.Domain.Entities;
+using MyCompany.Models;
+
+namespace MyCompany.Infrastructure
+{
+    public static class HelperDTO
+    {
+        //Service=>ServiceDTO
+        public static ServiceDTO TransformService(Service entity)
+        {
+            ServiceDTO entityDTO = new ServiceDTO();   
+            entityDTO.Id= entity.Id;
+            entityDTO.CategoryName=entity.ServiceCategory?.Title;
+            entityDTO.Title = entity.Title;
+            entityDTO.Description = entity.Description;
+            entityDTO.DescriptionShort= entity.DescriptionShort;
+            entityDTO.PhotoFileName = entity.Photo;
+            entityDTO.Type = entity.Type.ToString();
+
+            return entityDTO;
+
+        }
+    }
+}
---
+++ Models/ServiceDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MyCompany.Models
+{
+    //предназначен для передачи на клиент. скрывает важную информацию которую пользователю видеть не нужно
+    public class ServiceDTO
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? DescriptionShort { get; set; }
+        public string? PhotoFileName { get; set; }
+        public string? Type { get; set; }
+    }
+}
