1f77561b654006fe7dbcb5b9188a2be0df636160
dovlatyanc <dovlatyanc.ru@gmail.com>
2025-08-01 17:17:23 +0300

    наметил навигацию по сайту

--- Models/Components/MenuViewComponent.cs
+++ Models/Components/MenuViewComponent.cs
@@ -1,8 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using MyCompany.Domain.Entities;
 
 namespace MyCompany.Models.Components
 {
     public class MenuViewComponent:ViewComponent
     {
+        private readonly DataManager _dataManager;
+
+        public MenuViewComponent(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        private async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Service> list = await _dataManager.Services.GetServiceAsync();
+            return await Task.FromResult((IViewComponentResult) View("Default",list));
+        }
     }
 }
